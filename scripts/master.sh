d0afe8a45bfd58e6e6987f72f21db315757a9b1f
Owain Lewis <owain.lewis@oracle.com>
2018-10-08 13:05:49 +0100

    Update master.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -2,39 +2,6 @@
 
 echo "Provisioning K8s master"
 
-# Update system
-yum update -y
-
-# Disable SELinux
-setenforce 0
-
-# Remove existing Docker packages
-yum remove docker \
-           docker-client \
-           docker-client-latest \
-           docker-common \
-           docker-latest \
-           docker-latest-logrotate \
-           docker-logrotate \
-           docker-selinux \
-           docker-engine-selinux \
-           docker-engine
-
-# Install Docker prerequisites 
-yum install -y \
-    yum-utils \
-    device-mapper-persistent-data \
-    lvm2
-
-# Install Docker CE
-yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo    
-yum install -y docker-ce
-
-# Add current user to Docker group
-usermod -aG docker $(whoami)
-
 # Install K8s packages
 bash -c 'cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
