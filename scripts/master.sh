e7939a5b522d9ad098d444a79682b21baab05d61
Owain Lewis <owain.lewis@oracle.com>
2018-10-08 12:30:10 +0100

    Create master.sh

---
+++ scripts/master.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Update system
+yum update -y
+
+# Disable SELinux
+setenforce 0
+
+# Install Packages
+bash -c 'cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF'
+
+yum install -y kubelet kubeadm kubectl
+
+systemctl enable kubelet && sudo systemctl start kubelet
+
+# Manage firewall
+systemctl disable firewalld && systemctl stop firewalld
