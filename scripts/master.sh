4f5182c2f8514fb8d1099a8410b41b1f345d3448
Owain Lewis <owain.lewis@oracle.com>
2018-10-08 12:31:51 +0100

    Update master.sh

--- scripts/master.sh
+++ scripts/master.sh
@@ -6,7 +6,34 @@ yum update -y
 # Disable SELinux
 setenforce 0
 
-# Install Packages
+# Remove existing Docker packages
+yum remove docker \
+           docker-client \
+           docker-client-latest \
+           docker-common \
+           docker-latest \
+           docker-latest-logrotate \
+           docker-logrotate \
+           docker-selinux \
+           docker-engine-selinux \
+           docker-engine
+
+# Install Docker prerequisites 
+yum install -y \
+    yum-utils \
+    device-mapper-persistent-data \
+    lvm2
+
+# Install Docker CE
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo    
+yum install -y docker-ce
+
+# Add current user to Docker group
+usermod -aG docker $(whoami)
+
+# Install K8s packages
 bash -c 'cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
