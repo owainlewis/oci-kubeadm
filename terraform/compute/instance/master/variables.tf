47c044fade9fda4b74904d3504fec4458dd8e2e6
Owain Lewis <owain.lewis@oracle.com>
2018-10-31 14:48:08 +0000

    Update variables.tf

--- terraform/compute/instance/master/variables.tf
+++ terraform/compute/instance/master/variables.tf
@@ -13,12 +13,6 @@ variable "oracle_linux_image_name" {
   default = "Oracle-Linux-7.5-2018.09.25-0"
 }
 
-variable "ssh_private_key" {
-  description = "SSH private key used for instances (generated if left blank)"
-  type        = "string"
-  default     = ""
-}
-
 variable "ssh_public_key" {
   description = "SSH public key in OpenSSH authorized_keys format for instances (generated if left blank)"
   type        = "string"
